fda26216c8cd85b323eb0399eb0e8b22be3247fd
ikeyu0806 <ikeyu0806@gmail.com>
2020-12-26 15:29:49 +0900

    show wrong date on the table

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,10 @@
         <tr>
           <th v-for="week in weeks" v-bind:key="week.id">{{ week.value }}</th>
         </tr>
-        <tr v-for="n of 6" v-bind:key="n">
-          <td v-for="n of 7" v-bind:key="n">{{ startDay }}</td>
+        <tr v-for="(week, index) in calendars" :key="index">
+          <td v-for="(day, index) in week" :key="index">
+            {{ day.date }}
+          </td>
         </tr>
       </thead>
     </table>
@@ -14,42 +16,65 @@
 </template>
 
 <script>
-
+/* eslint-disable */
 export default {
   data () {
     return {
       weeks: [
-        { id: 1, value: '月' },
-        { id: 2, value: '火' },
-        { id: 3, value: '水' },
-        { id: 4, value: '木' },
-        { id: 5, value: '金' },
-        { id: 6, value: '土' },
-        { id: 7, value: '日' }
+        { id: 1, value: 'MONDAY' },
+        { id: 2, value: 'TUESDAY' },
+        { id: 3, value: 'WEDNESDAY' },
+        { id: 4, value: 'THURSDAY' },
+        { id: 5, value: 'FRIDAY' },
+        { id: 6, value: 'SATURDAY' },
+        { id: 7, value: 'SUNDAY' }
       ]
     }
   },
   computed: {
+    calendars () {
+      return this.renderCalendar()
+    }
+  },
+  methods: {
     date () {
       return new Date()
     },
     year () {
-      return this.date.getFullYear()
+      return this.date().getFullYear()
     },
     month () {
-      return this.date.getMonth()
-    },
-    startDate () {
-      return new Date(this.year, this.month - 1, 1)
-    },
-    endDate () {
-      return new Date(this.year, this.month, 0)
+      return this.date().getMonth()
     },
     endDayCount () {
-      return this.endDate.getDay()
+      return this.endDate().getDay()
     },
     startDay () {
-      return this.startDate.getDay()
+      return this.startDate().getDay()
+    },
+    startDate () {
+      return new Date(this.year(), this.month() - 1, 1)
+    },
+    endDate () {
+      return new Date(this.year(), this.month(), 0)
+    },
+    renderCalendar () {
+      let startDate = this.startDate()
+      let currentDate = startDate
+      const calendars = []
+      for(let i=0;i<7;i++){
+        let weekRow = []
+        for (let day = 0; day < 7; day++) {
+          weekRow.push({
+            date: currentDate.getDate()
+          })
+          currentDate.setDate(currentDate.getDate() + 1)
+          console.log(currentDate)
+          console.log(weekRow)
+        }
+        calendars.push(weekRow)
+      }
+      return calendars
     }
   },
   components: {
