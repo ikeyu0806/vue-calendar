71b4ed85317ea16cc509b15919f9468841b839f8
ikeyu0806 <ikeyu0806@gmail.com>
2021-01-01 11:40:42 +0900

    add Docker file

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,7 @@ export default {
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint: 'http://localhost:4000',
+        httpEndpoint: 'http://localhost:3000',
       }
     }
   },
